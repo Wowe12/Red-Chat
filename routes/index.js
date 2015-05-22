7859c6bf9f51a999c8581ebbde107ddc38292cc6
Wowe12 <adjkkl@gmail.com>
2015-05-22 19:28:33 -0400

    V1

---
+++ routes/index.js
@@ -0,0 +1,37 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+/* GET home page. */
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('a user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('there has been a new message')
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+
+});
+
